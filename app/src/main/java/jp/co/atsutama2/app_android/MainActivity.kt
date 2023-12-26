48ccc727987260d194b898dc8e64abca1c3736a2
atsutama2 <atsutama.0713@gmail.com>
2023-12-26 14:31:17 +0900

    add sample code

--- app/src/main/java/jp/co/atsutama2/app_android/MainActivity.kt
+++ app/src/main/java/jp/co/atsutama2/app_android/MainActivity.kt
@@ -3,12 +3,18 @@ package jp.co.atsutama2.app_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,55 +26,125 @@ class MainActivity : ComponentActivity() {
         setContent {
             App_androidTheme {
                 // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background,
-//                ) {
-//                    Greeting("Android & iOS")
-//                }
-                MyApp(modifier = Modifier.fillMaxSize())
+                Surface {
+                    MyApp()
+                }
             }
         }
     }
 }
 
 @Composable
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text (text = "Hello $name!", Modifier.padding(24.dp))
-    }
-}
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
 
-@Composable
-private fun MyApp(modifier: Modifier = Modifier) {
     Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Greeting("Android TV")
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true , name = "Text Android")
+
+@Preview
 @Composable
-fun GreetingAndroidPreview() {
-    App_androidTheme {
-        Greeting("Android")
+fun GreetingPreview() {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello, ")
+                Text(text = "Atsutama!")
+            }
+            ElevatedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true, name = "Text iOS")
-@Composable
-fun GreetingIosPreview() {
-    App_androidTheme {
-        Greeting("iOS")
-    }
-}
+//@Composable
+//fun Greeting(name: String) {
+//    Surface(color = MaterialTheme.colorScheme.primary) {
+//        Text (text = "Hello $name!", Modifier.padding(24.dp))
+//    }
+//}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    App_androidTheme {
-        MyApp()
-    }
-}
+//@Composable
+//private fun MyApp(modifier: Modifier = Modifier) {
+//    Surface(
+//        modifier = modifier,
+//        color = MaterialTheme.colorScheme.background
+//    ) {
+//        Greeting("Android TV")
+//    }
+//}
+
+//@Preview(showBackground = true , name = "Text Android")
+//@Composable
+//fun GreetingAndroidPreview() {
+//    App_androidTheme {
+//        Greeting("Android")
+//    }
+//}
+
+//@Preview(showBackground = true, name = "Text iOS")
+//@Composable
+//fun GreetingIosPreview() {
+//    App_androidTheme {
+//        Greeting("iOS")
+//    }
+//}
+
+//@Preview(showBackground = true, widthDp = 320)
+//@Composable
+//fun Default1Preview() {
+//    App_androidTheme {
+//        MyApp()
+//    }
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun Default2Preview() {
+//    App_androidTheme {
+//        MyApp()
+//    }
+//}
