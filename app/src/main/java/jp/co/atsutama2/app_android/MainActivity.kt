33decf43053a0ae3f1e2ef767056c89c14c1cf8d
atsutama2 <atsutama.0713@gmail.com>
2023-12-26 13:42:49 +0900

    add preview test

--- app/src/main/java/jp/co/atsutama2/app_android/MainActivity.kt
+++ app/src/main/java/jp/co/atsutama2/app_android/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import jp.co.atsutama2.app_android.ui.theme.App_androidTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,28 +20,38 @@ class MainActivity : ComponentActivity() {
         setContent {
             App_androidTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-                    Greeting("Android")
-                }
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background,
+//                ) {
+//                    Greeting("Android & iOS")
+//                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text (text = "Hello $name!", Modifier.padding(24.dp))
+    }
+}
+
+@Composable
+private fun MyApp(modifier: Modifier = Modifier) {
+    Surface(
         modifier = modifier,
-    )
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Greeting("Android TV")
+    }
 }
 
 @Preview(showBackground = true , name = "Text Android")
 @Composable
-fun GreetingAndroidPrevie() {
+fun GreetingAndroidPreview() {
     App_androidTheme {
         Greeting("Android")
     }
@@ -49,6 +61,14 @@ fun GreetingAndroidPrevie() {
 @Composable
 fun GreetingIosPreview() {
     App_androidTheme {
-        Greeting(name = "iOS")
+        Greeting("iOS")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    App_androidTheme {
+        MyApp()
     }
 }
